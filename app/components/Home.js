cc7916a6480e2814a9a421ceb1d99b6739e719de
Justin Seiter <justinseiter@gmail.com>
2016-05-18 19:13:47 -0500

    Add site dir structure.

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,21 +1,68 @@
 import React from 'react';
+import Logo from '../img/bull.svg';
 
 const Home = () => {
   return (
-    <div className="container is-fluid">
-      <div className="columns">
-        <div className="column is-success">
-          First column
+    <div className="">
+      <nav className="nav">
+        <div className="container">
+        <div className="nav-left">
+          <a className="nav-item logo" href="#">
+            <img src={Logo} /><span>Monitaur</span>
+          </a>
         </div>
-        <div className="column">
-          Second column
+
+        <span className="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div className="nav-right nav-menu">
+          <a className="nav-item" href="#">
+            Documentation
+          </a>
+          <a className="nav-item" href="#">
+            Support
+          </a>
         </div>
-        <div className="column">
-          Third column
         </div>
-        <div className="column">
-          Fourth column
-        </div>
+      </nav>
+      <div className="container">
+      <div className="tabs is-centered">
+        <ul>
+          <li className="is-active">
+            <a>
+              <span className="icon is-small"><i className="fa fa-dashboard "></i></span>
+              <span>Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <span className="icon is-small"><i className="fa fa-twitter"></i></span>
+              <span>Twitter</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <span className="icon is-small"><i className="fa fa-instagram"></i></span>
+              <span>Instagram</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <span className="icon is-small"><i className="fa fa-tumblr"></i></span>
+              <span>Tumblr</span>
+            </a>
+          </li>
+          <li>
+            <a>
+              <span className="icon is-small"><i className="fa fa-facebook"></i></span>
+              <span>Facebook</span>
+            </a>
+          </li>
+        </ul>
+      </div>
       </div>
     </div>
   );
--- app/components/Main.js
+++ app/components/Main.js
@@ -3,35 +3,13 @@ import React, { PropTypes } from 'react';
 const Main = ({ children }) => {
   return (
     <div>
-      <nav className="nav">
-        <div className="nav-left">
-          <a className="nav-item" href="#">
-            Monitaur
-          </a>
-        </div>
-
-        <span className="nav-toggle">
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-
-        <div className="nav-right nav-menu">
-          <a className="nav-item" href="#">
-            Documentation
-          </a>
-          <a className="nav-item" href="#">
-            Support
-          </a>
-        </div>
-      </nav>
       {children}
     </div>
   );
 };
 
 Main.propTypes = {
-  children: PropTypes.object,
+  children: PropTypes.node.isRequired,
 };
 
 export default Main;
